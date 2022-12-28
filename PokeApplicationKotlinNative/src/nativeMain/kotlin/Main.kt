4dc8f812190e0a27bff83c4248947ede3ca30413
Anton Saatze <a.saatze@googlemail.com>
2022-12-28 16:57:40 +0100

    Add gitignore to Application-Kotlin/Native and cancel application on SIGINT signal

--- PokeApplicationKotlinNative/src/nativeMain/kotlin/Main.kt
+++ PokeApplicationKotlinNative/src/nativeMain/kotlin/Main.kt
@@ -1,11 +1,23 @@
+import kotlinx.cinterop.staticCFunction
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import platform.posix.SIGINT
+import platform.posix.exit
+import platform.posix.signal
 import platform.posix.sleep
 import redtoss.poke.lib.PokeApiClient
 
 const val LOG_PREFIX = "Kotlin/Native:"
 fun main() {
+    signal(SIGINT, staticCFunction<Int,Unit> { signal->
+        when(signal){
+            SIGINT -> {
+                println("$LOG_PREFIX Exit by ctrl + c")
+                exit(signal)
+            }
+        }
+    })
     var isDone = false
     println("$LOG_PREFIX Type in a Pokemon name like Pikachu, bulbasaur, charmander, squirtle or type 'exit' to stop the program")
     val api = PokeApiClient(logger = Logger())
