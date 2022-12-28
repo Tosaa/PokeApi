b275bc1bfc396d123afd9efe8ded05aa0de9d9e3
Anton Saatze <a.saatze@googlemail.com>
2022-12-28 16:42:42 +0100

    update C-Application code, but still doesnt work

--- PokeApplicationC/readme.md
+++ PokeApplicationC/readme.md
@@ -11,9 +11,20 @@ Be aware that curl needs to be available on the system.
 `mkdir [build folder]`
 `cd [build folder]`
 `cmake ..`
-`make .`
+`make`
 
 ### run
 The program should be a CLI, but currently its just searching for 2 Pokemon and exits then again.
 
 `./PokeNativeC`
+
+
+### Caution:
+This application is currently not usable. 
+Right now its unclear to me how certain variables and methods can be passed.
+e.g. The Logger inherits from ILogger. In the C there is nothing like inheritance.
+Thats why the assumption is made, to reassign the `ILogger.pinned` variable.
+But this is not working.
+
+Actually I could not find any documentation how a Kotlin/Native library can be called from C.
+One last option is to create a custom .def file which defines some typealias for functions.
--- PokeApplicationC/src/main.c
+++ PokeApplicationC/src/main.c
@@ -3,23 +3,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-void *print(char *str) {
-    printf("%s\n", str);
+void custom_print(char *str) {
+    printf("Main: %s\n", str);
+    return;
 }
 
 int main() {
-    printf("Hello World\n");
+    custom_print("Hello World");
     //obtain reference for calling Kotlin/Native functions
     libpokeapi_ExportedSymbols *lib = libpokeapi_symbols();
+
     libpokeapi_kref_kotlin_Function1 logging_function;
-    logging_function.pinned = (void *) print;
-    libpokeapi_kref_redtoss_poke_lib_logging_Logger logger;
-    lib->kotlin.root.redtoss.poke.lib.logging.Logger.set_loggingFunction(logger, logging_function);
+    logging_function.pinned = (void *) custom_print;
+
+    libpokeapi_kref_redtoss_poke_lib_logging_Logger logger = lib->kotlin.root.redtoss.poke.lib.logging.Logger.Logger();
+    lib->kotlin.root.redtoss.poke.lib.logging.Logger.set_loggingFunction(logger, custom_print);
+
     libpokeapi_kref_redtoss_poke_lib_logging_ILogger ilogger;
     ilogger.pinned = logger.pinned;
 
     _Bool cachingEnabled = 1;
-    libpokeapi_kref_redtoss_poke_lib_PokeApiClient apiClient = lib->kotlin.root.redtoss.poke.lib.createDefaultPokeApiClient(            ilogger, cachingEnabled);
+    libpokeapi_kref_redtoss_poke_lib_PokeApiClient apiClient = lib->kotlin.root.redtoss.poke.lib.PokeApiClient.PokeApiClient(ilogger, cachingEnabled);
     lib->kotlin.root.redtoss.poke.lib.findPokemonOnCurrentThread(apiClient, "Pikachu");
     return 0;
 }
